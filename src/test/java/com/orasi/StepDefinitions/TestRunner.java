266612c62e4ec62bd8beee81896fa7dad2e84d63
Orasi_SA <orasisa@orasi.com>
2017-04-20 13:04:12 -0400

    making twaks to the test so I can get reports

--- src/test/java/com/orasi/StepDefinitions/TestRunner.java
+++ src/test/java/com/orasi/StepDefinitions/TestRunner.java
@@ -8,13 +8,15 @@ import org.junit.runner.RunWith;
 @RunWith(OctaneCucumber.class)
 @CucumberOptions(
         features = "src/test/java/com/orasi/Features",
+        //glue the script to a package where the step definitions can be found.
         glue = "com.orasi.StepDefinitions",
         plugin = {
                 "pretty",
-                "json:RunResults/cucmber.json",
-                "junit:RunResults/cucmber.xml",
-                "junit:RunResults/cucmber.html"
+                "json:RunResults/runresults.json",
+                "junit:RunResults/runresults.xml",
+                "junit:RunResults/runresults.html"
         },
+        //provide the tag needed by Octane
         tags = {"@TID42009REV0.4.0"}
 )
 
